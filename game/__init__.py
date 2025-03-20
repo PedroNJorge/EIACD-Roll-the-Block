be3d0efcd83a8c806a25addc7e538cf9788efe21
PedroNJorge <pedro.nobre.jorge@gmail.com>
2025-03-20 17:46:15 +0000

    files

---
+++ game/__init__.py
@@ -0,0 +1,5 @@
+from .board import Board
+from .block import Block
+from .game_logic import GameLogic
+from .renderer import Renderer
+from .levels import Levels
---
+++ game/board.py
@@ -0,0 +1,11 @@
+import Levels
+
+
+class Board:
+    def __init__(self, level_name):
+        self.level = Levels(level_name)
+        self.width = len(self.level.layout[0])
+        self.height = len(self.level.layout)
+
+    def is_goal(self, position):
+        return self.level.is_goal(position)
