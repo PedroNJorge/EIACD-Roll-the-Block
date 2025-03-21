4099f09a089f216ad7257b2c81da4140a4622fcc
anaaaraujoo <francisca.araujo1210@gmail.com>
2025-03-21 10:36:26 +0000

    Update input_handler.py

--- game/input_handler.py
+++ game/input_handler.py
@@ -14,7 +14,7 @@ class InputHandler:
                     return False    # used to update run in main.py
 
         return True
-
+        
     def handle_keyboard(self, event):
         match(event.key):
             case pygame.K_w:
@@ -24,15 +24,69 @@ class InputHandler:
                         self.board.level.layout[self.block.x - 1][self.block.y] = 2
                         self.board.level.layout[self.block.x - 2][self.block.y] = 2
 
+                    case "horizontal":
+                        self.board.level.layout[self.block.x + 1][self.block.y] = 0
+                        self.board.level.layout[self.block.x - 1][self.block.y] = 1
+
+                    case "vertical":
+                        self.board.level.layout[self.block.x][self.block.y + 1] = 0
+                        self.board.level.layout[self.block.x - 1][self.block.y] = 2
+                        self.board.level.layout[self.block.x - 1][self.block.y + 1] = 2
+
                 self.block.move("up")
 
             case pygame.K_s:
+                self.board.level.layout[self.block.x][self.block.y] = 0
+                match(self.block.orientation):
+                    case "upright":
+                        self.board.level.layout[self.block.x + 1][self.block.y] = 2
+                        self.board.level.layout[self.block.x + 2][self.block.y] = 2
+
+                    case "horizontal":
+                        self.board.level.layout[self.block.x + 1][self.block.y] = 0
+                        self.board.level.layout[self.block.x + 2][self.block.y] = 1
+                    
+                    case "vertical":
+                        self.board.level.layout[self.block.x][self.block.y + 1] = 0
+                        self.board.level.layout[self.block.x + 1][self.block.y] = 2
+                        self.board.level.layout[self.block.x + 1][self.block.y + 1] = 2
+
                 self.block.move("down")
 
             case pygame.K_a:
+                self.board.level.layout[self.block.x][self.block.y] = 0
+                match(self.block.orientation):
+                    case "upright":
+                        self.board.level.layout[self.block.x][self.block.y - 1] = 2
+                        self.board.level.layout[self.block.x][self.block.y - 2] = 2
+
+                    case "horizontal":
+                        self.board.level.layout[self.block.x + 1][self.block.y] = 0
+                        self.board.level.layout[self.block.x][self.block.y - 1] = 2
+                        self.board.level.layout[self.block.x + 1][self.block.y - 1] = 2
+                    
+                    case "vertical":
+                        self.board.level.layout[self.block.x][self.block.y - 1] = 0
+                        self.board.level.layout[self.block.x][self.block.y - 2] = 1
+
                 self.block.move("left")
 
             case pygame.K_d:
+                self.board.level.layout[self.block.x][self.block.y] = 0
+                match(self.block.orientation):
+                    case "upright":
+                        self.board.level.layout[self.block.x][self.block.y + 1] = 2
+                        self.board.level.layout[self.block.x][self.block.y + 2] = 2
+
+                    case "horizontal":
+                        self.board.level.layout[self.block.x + 1][self.block.y] = 0
+                        self.board.level.layout[self.block.x][self.block.y + 1] = 2
+                        self.board.level.layout[self.block.x + 1][self.block.y + 1] = 2
+                    
+                    case "vertical":
+                        self.board.level.layout[self.block.x][self.block.y + 1] = 0
+                        self.board.level.layout[self.block.x][self.block.y + 2] = 1
+                        
                 self.block.move("right")
 
         self.game_logic.update()
