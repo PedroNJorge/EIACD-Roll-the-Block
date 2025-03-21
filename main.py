205720646d19b173bdfcafe39174fbabd72f8306
Pedro Jorge <110513576+PedroNJorge@users.noreply.github.com>
2025-03-21 10:39:59 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import pygame
 from game import Board
 from game import Block
 from game import GameLogic
-from game import Renderer
 from game import InputHandler
+from game import Levels
+from game import Renderer
 
 
 def main():
@@ -14,6 +15,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Roll the Block")
+
+    board = Board("LEVEL1")
+    block = Block(board.level.start[0], board.level.start[1])
+    game_logic = GameLogic(block, board)
+    input_handler = InputHandler(block, board, game_logic)
+    renderer = Renderer(block, board)
 
     while run:
         #use input handler
